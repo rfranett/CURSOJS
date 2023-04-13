b2e9fee1adaeea86db747badcc5083cb9fd31d00
rfranett <rfranett@gmail.com>
2023-04-13 16:13:07 -0300

    treino

--- function.js
+++ function.js
@@ -1,8 +1,9 @@
 /*funções 
 ex 
 */
-
-
+let saldo = 10000
+let divida = 100
+const minhaConta =2876566-9
 const saldobc = 2000
 const saldodv = 100
 
@@ -10,5 +11,72 @@ if (saldobc > saldodv ){
     console.log("sua conta foi paga com sucesso")
 } else {
     console.log("parede mimimis")
-    
+   
 }
+function meuNome (){
+    console.log("robson")
+}
+console.log("um")
+console.log(minhaConta)
+meuNome()
+console.log(saldo - divida)
+
+
+function divisao (p1,p2){
+
+    const divisao = p1 / p2
+
+    console.log(divisao.toFixed(2))
+}
+
+divisao (30,7)
+
+function menos (p3,p4){
+
+    const menos = p3 - p4
+
+    console.log(menos)
+}
+
+menos (30,7)
+
+function xx (p1,p2){
+
+    const xx = p1 * p2
+
+    console.log(xx)
+}
+
+xx (30,7)
+
+function mais (p1,p2){
+
+    const mais = p1 + p2
+
+    console.log(mais)
+}
+
+mais (30,55)
+mais (30,56)
+mais (30,58)
+
+function multiplic (n1,n2){ 
+
+    const resultado = n1 * n2
+
+    return resultado
+}
+
+console.log(multiplic(2,5))
+console.log(multiplic(222,5))
+console.log(multiplic(12,53))
+
+function menos2 (n1,n2){
+
+    const result =n1 - n2
+
+    return result
+}
+ console.log(menos2(436,7))
+ console.log(menos2(443,3))
+ console.log(menos2(343,5))
